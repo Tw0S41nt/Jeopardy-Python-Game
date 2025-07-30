874642311fde8b69cf07594208e97674f7f91092
lefkovitzj <85521235+lefkovitzj@users.noreply.github.com>
2025-07-29 21:05:49 -0400

    Add basic flask webserver setup

---
+++ app.py
@@ -0,0 +1,14 @@
+"""
+Project Name: Jeopardy Python Game
+Project Author(s): 
+    Joseph Lefkovitz (github.com/lefkovitz),
+    Santiago Rached Alvarez (https://github.com/Tw0S41nt)
+Last Modified: 7/29/2025
+
+File Purpose: Web application backend logic.
+"""
+
+from web import app
+
+if __name__ == "__main__":
+    app.run()
---
+++ web.py
@@ -0,0 +1,34 @@
+"""
+Project Name: Jeopardy Python Game
+Project Author(s): 
+    Joseph Lefkovitz (github.com/lefkovitz),
+    Santiago Rached Alvarez (https://github.com/Tw0S41nt)
+Last Modified: 7/29/2025
+
+File Purpose: Web application backend logic.
+"""
+
+# Standard library imports.
+import os
+
+# Third-party imports.
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Local application imports.
+
+# Create the application.
+app = Flask(__name__)
+load_dotenv()
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+# Configure the database.
+db = SQLAlchemy()
+db.init_app(app)
+
+@app.route("/")
+def homepage():
+    """ Display the application homepage. """
+    return "This... is Jeopardy!"
