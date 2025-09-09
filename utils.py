75aa698cae00b330ac2dd55c958990581e43537c
lefkovitzj <85521235+lefkovitzj@users.noreply.github.com>
2025-09-08 20:39:01 -0400

    Enable round-specific data requests

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ File Purpose: Data manipulation and processing utilities for the application.
 import csv
 import random
 
-def get_all_category_entries(data_source):
+def get_all_category_entries(data_source, disallowed_rounds=[3]):
     """ Parse the source file and get a list of all unique categories that are not in the Final Jeopardy."""
     # Sanity check.
     assert data_source.endswith(".tsv")
@@ -22,11 +22,11 @@ def get_all_category_entries(data_source):
         jeopardy_categories = []
         for jeopardy_question_data in jeopardy_data:
             # Select all question categories that are unique and not Final Jeopardy. 
-            if jeopardy_question_data[3] not in jeopardy_categories and int(jeopardy_question_data[0]) != 3:
+            if jeopardy_question_data[3] not in jeopardy_categories and int(jeopardy_question_data[0]) not in disallowed_rounds:
                 jeopardy_categories.append(jeopardy_question_data[3])
     return jeopardy_categories, len(jeopardy_categories)
-        
-def get_all_question_entries(data_source):
+
+def get_all_question_entries(data_source, disallowed_rounds=[3]):
     """  Parse the source file and get the questions, points, answers, and categories that are not in the Final Jeopardy."""
     # Sanity check.
     assert data_source.endswith(".tsv")
@@ -39,7 +39,7 @@ def get_all_question_entries(data_source):
         jeopardy_questions = []
         for jeopardy_question_data in jeopardy_data:
             # Select all of the question data for questions not in Final Jeopardy.
-            if int(jeopardy_question_data[0]) !=3:
+            if int(jeopardy_question_data[0]) not in disallowed_rounds:
                 question = jeopardy_question_data[5]
                 points = jeopardy_question_data[1] 
                 answer = jeopardy_question_data[6]
@@ -49,11 +49,11 @@ def get_all_question_entries(data_source):
                 jeopardy_questions.append(question_dict)
     return jeopardy_questions, len(jeopardy_questions)
 
-def get_random_game_board(data_source, number_categories=6, number_questions=5):
+def get_random_game_board(data_source, number_categories=6, disallowed_rounds = [3]):
     """ Build the jeopardy game board with random categories and questions."""
 
-    all_questions, _ = get_all_question_entries(data_source)
-    all_categories, _ = get_all_category_entries(data_source)
+    all_questions, _ = get_all_question_entries(data_source, disallowed_rounds=disallowed_rounds)
+    all_categories, _ = get_all_category_entries(data_source, disallowed_rounds=disallowed_rounds)
 
     board_categories = random.sample(all_categories, number_categories)
     question_list = []
